a8a06c8a291954a311379f02b85b353d4c2349f1
vagrant <vagrant@homestead>
2019-01-23 14:54:34 +0000

    add models

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = ['name'];
+    
+    public function article(){
+        return $this->belongsToMany(Article::class, 'article_tag', 'tag_id', 'article_id')->withTimestamps();
+    }
 }
