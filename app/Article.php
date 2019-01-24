6819b59731f6902f7b7943449afa8baedef64ed2
gafasvvv <ry0suke.programming@gmail.com>
2019-01-24 10:25:00 +0000

    add models

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = ['title', 'body', 'photo'];
+
+    public function tag(){
+        return $this->belongsToMany(Tag::class, 'article_tag', 'tag_id', 'article_id')->withTimestamps();
+    }
 }
